fb5e8e86cb98137eee9def14b926c62edd90fe39
Bj?rn Fahller <bjorn@fahller.se>
2018-12-25 15:44:37 +0100

    Removed #ifdef:d out code.

--- include/typical.hpp
+++ include/typical.hpp
@@ -431,7 +431,7 @@ struct filter
 
 template<typename P, typename L>
 using filter_t = apply<filter<P>, L>;
-#if 1
+
 template <typename T>
 struct wrapped;
 
@@ -450,99 +450,6 @@ struct drop_first
     using f = decltype(drop_first_<make_n<type_list, N::value, const void*>>::func(static_cast<wrapped<Ts>*>(nullptr)...));
 };
 
-#else
-template <typename L, typename C = make<type_list>>
-struct drop_first
-{
-  template <std::size_t I, std::size_t N, bool, typename ...>
-  struct helper;
-  template <typename ... Ts>
-  struct helper<0,0, false, Ts...>
-  {
-    using type = apply<C, Ts...>;
-  };
-  template <typename T, typename ... Ts>
-  struct helper<1,0, false, T, Ts...>
-  {
-    using type = apply<C, Ts...>;
-  };
-  template <std::size_t I, typename T1, typename T2, typename ... Ts>
-  struct helper<I, 1, false, T1,T2, Ts...>
-  {
-    using type = typename helper<I-2, log2(I-2), false, Ts...>::type;
-  };
-  template <std::size_t I,
-            typename T1, typename T2, typename T3, typename T4,
-            typename ... Ts>
-  struct helper<I, 2, false, T1,T2,T3,T4, Ts...>
-  {
-    using type = typename helper<I-4, log2(I-4), false, Ts...>::type;
-  };
-  template <std::size_t I,
-            typename T1, typename T2, typename T3, typename T4,
-            typename T5, typename T6, typename T7, typename T8,
-            typename ... Ts>
-  struct helper<I, 3, false, T1,T2,T3,T4,T5,T6,T7,T8, Ts...>
-  {
-    using type = typename helper<I-8, log2(I-8), false, Ts...>::type;
-  };
-  template <std::size_t I,
-            typename T1, typename T2, typename T3, typename T4,
-            typename T5, typename T6, typename T7, typename T8,
-            typename T9, typename T10, typename T11, typename T12,
-            typename T13, typename T14, typename T15, typename T16,
-            typename ... Ts>
-  struct helper<I, 4, false, T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16, Ts...>
-  {
-    using type = typename helper<I-16, log2(I-16), false, Ts...>::type;
-  };
-  template <std::size_t I,
-            typename T1, typename T2, typename T3, typename T4,
-            typename T5, typename T6, typename T7, typename T8,
-            typename T9, typename T10, typename T11, typename T12,
-            typename T13, typename T14, typename T15, typename T16,
-            typename T17, typename T18, typename T19, typename T20,
-            typename T21, typename T22, typename T23, typename T24,
-            typename T25, typename T26, typename T27, typename T28,
-            typename T29, typename T30, typename T31, typename T32,
-            typename ... Ts>
-  struct helper<I, 5, false,
-                T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,
-                T17,T18,T19,T20,T21,T22,T23,T24,T25,T26,T27,T28,T29,T30,T31,T32,                Ts...>
-  {
-    using type = typename helper<I-32, log2(I-32), false, Ts...>::type;
-  };
-  template <std::size_t I, std::size_t N,
-            typename T1, typename T2, typename T3, typename T4,
-            typename T5, typename T6, typename T7, typename T8,
-            typename T9, typename T10, typename T11, typename T12,
-            typename T13, typename T14, typename T15, typename T16,
-            typename T17, typename T18, typename T19, typename T20,
-            typename T21, typename T22, typename T23, typename T24,
-            typename T25, typename T26, typename T27, typename T28,
-            typename T29, typename T30, typename T31, typename T32,
-            typename T33, typename T34, typename T35, typename T36,
-            typename T37, typename T38, typename T39, typename T40,
-            typename T41, typename T42, typename T43, typename T44,
-            typename T45, typename T46, typename T47, typename T48,
-            typename T49, typename T50, typename T51, typename T52,
-            typename T53, typename T54, typename T55, typename T56,
-            typename T57, typename T58, typename T59, typename T60,
-            typename T61, typename T62, typename T63, typename T64,
-            typename ...Ts>
-  struct helper<I,N,true,
-                T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,
-                T17,T18,T19,T20,T21,T22,T23,T24,T25,T26,T27,T28,T29,T30,T31,T32,
-                T33,T34,T35,T36,T37,T38,T39,T40,T41,T42,T43,T44,T45,T46,T47,T48,
-                T49,T50,T51,T52,T53,T54,T55,T56,T57,T58,T59,T60,T61,T62,T63,T64,
-                Ts...>
-  {
-    using type = typename helper<I-64, log2(I-64), (I>=128), Ts...>::type;
-  };
-  template <typename ... Ts>
-  using f = typename helper<L::value, log2(L::value), (L::value >= 64), Ts...>::type;
-};
-#endif
 
 template<typename P, typename C = make<type_list>>
 struct partition {
