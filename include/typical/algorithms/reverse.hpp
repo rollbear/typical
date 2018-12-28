4a82ed87d167935f5bda56badee3ab679ad2ecb7
Bj?rn Fahller <bjorn@fahller.se>
2018-12-28 20:33:04 +0100

    move reverse into own header/test

---
+++ include/typical/algorithms/reverse.hpp
@@ -0,0 +1,71 @@
+#ifndef TYPICAL_REVERSE_HPP
+#define TYPICAL_REVERSE_HPP
+
+#include <typical/function_support.hpp>
+#include <typical/application.hpp>
+#include <typical/algorithms/join.hpp>
+
+// TODO: This naive implementation is super slow!
+
+namespace typical
+{
+struct reverse {
+  using continuation = make<list>;
+  template<typename C = continuation>
+  struct to {
+    using TO = detail::to<C>;
+    template<std::size_t N, typename, bool>
+    struct helper;
+
+    template<template<typename ...> class L, typename ... Ts>
+    struct helper<0, L<Ts...>, false> {
+      using type = L<Ts...>;
+    };
+    template<template<typename...> class L, typename T, typename ... Ts>
+    struct helper<1, L<T, Ts...>, false> {
+      using type = L<T, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename ... Ts>
+    struct helper<2, L<T1, T2, Ts...>, false> {
+      using type = L<T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename ... Ts>
+    struct helper<3, L<T1, T2, T3, Ts...>, false> {
+      using type = L<T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename ... Ts>
+    struct helper<4, L<T1, T2, T3, T4, Ts...>, false> {
+      using type = L<T4, T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename ... Ts>
+    struct helper<5, L<T1, T2, T3, T4, T5, Ts...>, false> {
+      using type = L<T5, T4, T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename ... Ts>
+    struct helper<6, L<T1, T2, T3, T4, T5, T6, Ts...>, false> {
+      using type = L<T6, T5, T4, T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename ... Ts>
+    struct helper<7, L<T1, T2, T3, T4, T5, T6, T7, Ts...>, false> {
+      using type = L<T7, T6, T5, T4, T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename ... Ts>
+    struct helper<8, L<T1, T2, T3, T4, T5, T6, T7, T8, Ts...>, false> {
+      using type = L<T8, T7, T6, T5, T4, T3, T2, T1, Ts...>;
+    };
+    template<template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename ... Ts>
+    struct helper<9, L<T1, T2, T3, T4, T5, T6, T7, T8, T9, Ts...>, false> {
+      using type = L<T9, T8, T7, T6, T5, T4, T3, T2, T1, Ts...>;
+    };
+    template<std::size_t N, template<typename ...> class L, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename ... Ts>
+    struct helper<N, L<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Ts...>, true> {
+      using type = apply_pack_to<join, TO, typename helper<
+        N - 10, L<Ts...>, (N >= 20)>::type, L<T10, T9, T8, T7, T6, T5, T4, T3, T2, T1>>;
+    };
+    template<typename ...T>
+    using result = typename helper<sizeof...(T), typename TO::template result<T...>, (sizeof...(T) >= 10)>::type;
+  };
+};
+
+}
+#endif //TYPICAL_REVERSE_HPP
--- main.cpp
+++ main.cpp
@@ -68,48 +68,6 @@ static_assert(std::is_same_v<apply_pack<typical::partition<typical::is_pointer>,
   typical::list<typical::list<void*,int*,char*>,typical::list<int,double>>>);
 
 
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>>,
-  typical::list<c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>>,
-  typical::list<c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>>,
-  typical::list<c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>>,
-  typical::list<c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>>,
-  typical::list<c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>>,
-  typical::list<c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>>,
-  typical::list<c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>>,
-  typical::list<c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>>,
-  typical::list<c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>>,
-  typical::list<c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>>,
-  typical::list<c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>>,
-  typical::list<c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>>,
-  typical::list<c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>>,
-  typical::list<c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>>,
-  typical::list<c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>>,
-  typical::list<c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>,c<17>>,
-  typical::list<c<17>,c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>,c<17>,c<18>>,
-  typical::list<c<18>,c<17>,c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>,c<17>,c<18>,c<19>>,
-  typical::list<c<19>,c<18>,c<17>,c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>,c<17>,c<18>,c<19>,c<20>>,
-  typical::list<c<20>,c<19>,c<18>,c<17>,c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
-static_assert(std::is_same_v<apply_pack<typical::reverse, c<1>,c<2>,c<3>,c<4>,c<5>,c<6>,c<7>,c<8>,c<9>,c<10>,c<11>,c<12>,c<13>,c<14>,c<15>,c<16>,c<17>,c<18>,c<19>,c<20>,c<21>>,
-  typical::list<c<21>,c<20>,c<19>,c<18>,c<17>,c<16>,c<15>,c<14>,c<13>,c<12>,c<11>,c<10>,c<9>,c<8>,c<7>,c<6>,c<5>,c<4>,c<3>,c<2>,c<1>>>);
 
 static_assert(std::is_same_v<apply_pack<typical::flatten,
   typical::list<c<1>,c<2>,typical::list<c<3>, c<4>, typical::list<c<5>, c<6>>>,std::tuple<c<7>, c<8>, typical::list<c<9>,c<10>>>,typical::list<c<11>,typical::list<c<12>,typical::list<c<13>>>>>>,
