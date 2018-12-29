f3a3d14ee7eaeea8a8cacf413b1ac16b261edf6d
Bj?rn Fahller <bjorn@fahller.se>
2018-12-29 01:23:01 +0100

    Copyright/License

--- include/typical/algorithms/index_of.hpp
+++ include/typical/algorithms/index_of.hpp
@@ -1,3 +1,17 @@
+/*
+ * typical - C++ template meta programming library
+ *
+ * Copyright Björn Fahller 2018
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/typical
+ */
+
+
 #ifndef TYPICAL_INDEX_OF_HPP
 #define TYPICAL_INDEX_OF_HPP
 
